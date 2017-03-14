d14cf2bd3f10c0df74c2d303d09affe2be4cdabf
Tom Kobryn <tom.kobryn@gmail.com>
2017-03-14 13:22:23 +0000

    adding jquery ajax

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // Loading HTML partials
-$( document ).ready(function() {
+
 $( "#headerPartial" ).load( "partials/header.html" );
 $( "#footerPartial" ).load( "partials/footer.html" );
-});
+
 
 
 // JQuery AJAX
